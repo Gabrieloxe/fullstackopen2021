d80faa1d90e69ae9555cd241bb9d53d28f81eb50
Gabriel Ong <gabrielongxe@gmail.com>
2021-04-10 22:22:09 +0800

    right before ex 1.6

--- follow-along-app/src/App.js
+++ follow-along-app/src/App.js
@@ -1,51 +1,26 @@
 import React, { useState } from 'react';
 
-const History = ({allClicks}) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const Display = ({value}) => <div>{value}</div>;
 
-  return (
-    <div>
-      button press history: {allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+  const setToValue = (newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text='thousand' />
+      <Button handleClick={() => setToValue(0)} text='reset' />
+      <Button handleClick={() => setToValue(value + 1)} text='increment' />
     </div>
-  )
-}
+  );
+};
 
 export default App;
