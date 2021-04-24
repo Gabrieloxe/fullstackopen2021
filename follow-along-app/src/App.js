2bb7766afe785e7b0e1aead3051744d9a72f40f2
Gabriel Ong <gabrielongxe@gmail.com>
2021-04-24 17:03:04 +0800

    right before exercise 2.1

--- follow-along-app/src/App.js
+++ follow-along-app/src/App.js
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Note from './components/Note'
 
-const Display = ({value}) => <div>{value}</div>;
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>{text}</button>
-);
-
-const App = () => {
-  const [value, setValue] = useState(10);
-
-  const setToValue = (newValue) => {
-    setValue(newValue);
-  };
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text='thousand' />
-      <Button handleClick={() => setToValue(0)} text='reset' />
-      <Button handleClick={() => setToValue(value + 1)} text='increment' />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
   );
 };
