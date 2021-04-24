7da07f7f6734c813c39b6f7b41417a3b6feabd56
Gabriel Ong <gabrielongxe@gmail.com>
2021-04-24 17:59:18 +0800

    Exercise 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Total = ({ course }) => {
+  const all_parts = course.parts;
+  const totalExercises = all_parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  const { parts } = course;
+  const display = parts.map((part) => {
+    return <Part part={part} key={part.id} />;
+  });
+  return <div>{display}</div>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course ={course}/>
+      <Total course={course}/>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
