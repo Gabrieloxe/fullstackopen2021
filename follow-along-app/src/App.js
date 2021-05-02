fb8f82e81f0406bbf465f1168ce33490595ba5dd
Gabriel Ong <gabrielongxe@gmail.com>
2021-05-02 13:46:41 +0800

    before exercise 2.11

--- follow-along-app/src/App.js
+++ follow-along-app/src/App.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log('effect');
+
+    const eventHandler = (response) => {
+      console.log('promise fulfilled');
+      setNotes(response.data);
+    };
+
+    axios.get('http://localhost:3001/notes').then(eventHandler);
+  };
+  useEffect(hook, []);
+
+  console.log('render', notes.length, 'notes');
 
   const addNote = (event) => {
     event.preventDefault();
