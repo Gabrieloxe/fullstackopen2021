17c181e2474da0151eb49bb31516080cbe0320de
Gabriel Ong <gabrielongxe@gmail.com>
2021-05-16 20:41:08 +0800

    delete the form?

--- part2/phonebook/src/components/personForm.js
+++ part2/phonebook/src/components/personForm.js
@@ -1,44 +1,5 @@
 import React from "react";
 
-
-// class PersonForm extends React.Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       newName: '',
-//       newNumber: ''
-//     }
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event){
-//     const value = event.target.value;
-//     this.setState({... state , [event.target.name]:value})
-//   }
-
-//   handleSubmit(event){
-//     event.preventDefault();
-
-//   }
-//   render() {
-//     return (
-//       <form onSubmit={addContact}>
-//         <div>
-//           Name: <input defaultValue={this.state.newName} onChange={handleNameChange} />
-//         </div>
-//         <div>
-//           Number: <input defaultValue={this.state.newNumber} onChange={handleNumberChange} />
-//         </div>
-//         <div>
-//           <button type='submit'>add</button>
-//         </div>
-//       </form>
-//     );
-//   }
-// }
-
-
 const PersonForm = ({
     addContact,
     newName,
