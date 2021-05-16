4934d60d2f0616e9c12613fcfc6fd99d4908ecc6
Gabriel Ong <gabrielongxe@gmail.com>
2021-05-16 12:47:11 +0800

    before 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -15,46 +15,52 @@ const Country = ({ country }) => {
   const countryLanguages = country.languages.map((language) => {
     return <Language language={language} key={language.name} />;
   });
-    return (
-      <div>
-        <h1>{country.name}</h1>
-        <div>Capital: {country.capital}</div>
-        <div>Population: {country.population}</div>
-        <h3>Languages</h3>
-        <ul>{countryLanguages}</ul>
-        <img src={country.flag} alt='Country Flag' height='150' width='150'></img>
-      </div>
-    );
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>Capital: {country.capital}</div>
+      <div>Population: {country.population}</div>
+      <h3>Languages</h3>
+      <ul>{countryLanguages}</ul>
+      <img src={country.flag} alt='Country Flag' height='150' width='150'></img>
+    </div>
+  );
 };
 
 const Language = ({ language }) => {
   return <li>{language.name}</li>;
 };
 
-const NamesList = ({countries}) =>{
-  const names = countries.map((country) => {
-    return (<div>{country.name}</div>);
-  });
-  return <div>{names}</div>;
-}
+const CountryName = ({ country, show }) => {
+  return (
+    <div>
+      {country.name}
+      <button value={country.numericCode} onClick={show}>
+        {' '}
+        show{' '}
+      </button>
+    </div>
+  );
+};
 
-const Display = ({ countries }) => {
+const Display = ({ countries, handleShowChange }) => {
   if (countries.length > 10) {
     return <div>Too many matches specifiy another filter</div>;
   } else if (countries.length === 1) {
     const country = countries[0];
     return <Country country={country} key={country.numericCode} />;
   } else {
-    const numericCodes = countries.map(country => country.numericCode);
-    return <NamesList countries={countries} key={numericCodes}/>
+    return countries.map((country) => {
+      return <CountryName country={country} key={country.numericCode} show={handleShowChange}/>;
+    });
   }
 };
-
-
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState('');
+  const [showAll, setShowAll] = useState(true);
+  const [showVal, setShowVal] = useState(0);
 
   const hook = () => {
     console.log('effect');
@@ -73,14 +79,27 @@ const App = () => {
     setFilter(event.target.value);
   };
 
+  const handleShowChange = (event) => {
+    setShowVal(event.target.value);
+    setShowAll(!showAll);
+  };
+
   const filteredCountries = countries.filter((country) =>
     country.name.toLowerCase().includes(filter.toLowerCase())
   );
 
+  const countriesToDisplay = showAll
+    ? filteredCountries
+    : filteredCountries.filter((country) => country.numericCode === showVal);
+
   return (
     <div>
       <CountryFilter filter={filter} handleFilterChange={handleFilterChange} />
-      <Display countries={filteredCountries}/>
+      <Display
+        countries={countriesToDisplay}
+        handleShowChange={handleShowChange}
+        show={showAll}
+      />
     </div>
   );
 };
