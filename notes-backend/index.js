138ae27b0f5d0bb21265f6619860b3dab8ca5ba3
Gabriel Ong <gabrielongxe@gmail.com>
2021-06-20 14:54:24 +0800

    exercise 3.7 and 3.8

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -34,25 +34,12 @@ let notes = [
   },
 ];
 
-
-
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
-});
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((n) => n.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
 });
 
 const generateId = () => {
@@ -81,6 +68,16 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note);
   response.json(note);
 })
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((n) => n.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
 
 app.delete('/api/notes/:id', (request, response)=>{
     const id = Number(request.params.id);
